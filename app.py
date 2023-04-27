e1961a555ceee376e55b9413075ccfb9cee4ef02
Felipe <felipeg@gmail.com>
2023-04-27 12:35:29 -0400

    version v0.0.1 - adding some methods

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 class Person:
     def __init__(self, person_name, person_phone, person_email):
         self.name = person_name
@@ -8,21 +7,27 @@ class Person:
     def __str__(self):
         return f"Person\n- Name: {self.name}\n- Phone: {self.phone}\n- Email: {self.email}"
 
-    # def print_pretty(self):
-    #     print(f"Person\n- Name: {self.name}\n- Phone: {self.phone}\n- Email: {self.email}")
+
+people = []
 
 
-# person1 = Person('Ricky', '1-800-MRCAST', 'ricky@email.com')
-# print(person1)
+def add_person():
+    name = input("Person's name: ")
+    phone = input("Person's phone: ")
+    email = input("Person's email: ")
+
+    p1 = Person(name, phone, email)
+
+    print(p1)
+    people.append(p1)
 
 
-name = input("Person's name: ")
-phone = input("Person's phone: ")
-email = input("Person's email: ")
-
-p1 = Person(name,phone,email)
-
-print(p1)
+def print_people():
+    for p in people:
+        print(p)
 
 
-
+add_person()
+add_person()
+add_person()
+print_people()
